7f8a1a3e8bfa295516c393d53862d550b9d437ab
assel.iskenderova <assel.iskenderova@bsbnb.kz>
2026-03-31 09:27:45 +0500

    CommentInput addComment

--- lib/presentation/widgets/comment_input.dart
+++ lib/presentation/widgets/comment_input.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:threads_clone/presentation/bloc/comments/comments_cubit.dart';
+import 'package:threads_clone/presentation/bloc/comments/comments_state.dart';
 
 class CommentInput extends StatefulWidget {
   const CommentInput({super.key});
@@ -8,8 +11,35 @@ class CommentInput extends StatefulWidget {
 }
 
 class _CommentInputState extends State<CommentInput> {
+  final TextEditingController _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox.shrink();
+    return 
+    Padding(
+      padding: EdgeInsets.all(8),
+      child: BlocBuilder<CommentsCubit, CommentsState>(
+        builder: (context, state) {return Row(
+          children: [
+            CircleAvatar(radius: 20, child: Icon(Icons.person)),
+            SizedBox(width: 8),
+            Expanded(
+              child: TextField(
+                controller: _textController,
+                decoration: InputDecoration(
+                hintText: 'Напишите комментарий...',
+                
+                ),onChanged: (value) {
+                      context.read<CommentsCubit>().inputChanged(_textController.text);
+                    },
+              ),
+            ),
+            IconButton(onPressed: () {
+              context.read<CommentsCubit>().addComment();
+            }, icon: Icon(Icons.send)),
+          ],
+        );}
+      )
+    );
   }
 }
